84dc33f64b8b2e1dfd45dc2dd3cdf968a43d55a6
Dan <danlungu_87@yahoo.com>
2026-06-07 21:33:19 +0300

    Add autoscaling group with launch configuration

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
 # outputs.tf
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.example.public_ip
-}
-
 output "server_port_used" {
   description = "The port the server is listening on"
   value       = var.server_port
