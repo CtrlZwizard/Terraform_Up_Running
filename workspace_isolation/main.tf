794fa692038b5826b69f9bde00402f35a78945e8
Dan <danlungu_87@yahoo.com>
2026-06-15 18:13:39 +0300

    Worspaces isolation was deployed

---
+++ workspace_isolation/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "example" {
+  ami            = "ami-0fb653ca2d3203ac1"
+   instance_type = "t2.micro"
+}
+
+terraform {
+backend "s3" {
+# Replace this with your bucket name!
+bucket         = "terraform-up-and-running-tfstate-dan-2026"
+key            = "workspaces-example/terraform.tfstate"
+region         = "us-east-2"
+# Replace this with your DynamoDB table name!
+dynamodb_table = "terraform-up-and-running-locks"
+encrypt        = true
+ }
+}
